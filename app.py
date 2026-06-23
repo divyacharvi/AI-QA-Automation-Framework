03824218a207e7d9e9dfd25de9b216c2b179b5fb
Divya Charvi <divyacharvi2005@gmail.com>
2026-06-23 15:32:13 +0530

    Render deployment configuration

--- app.py
+++ app.py
@@ -59,5 +59,10 @@ def home():
         result=result
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
